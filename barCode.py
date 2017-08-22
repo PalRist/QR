7b639d0cbc5974e17191b823ab309556c8c233a3
Test <123@test.com>
2017-08-22 10:16:48 +0200

    UI-stuff

--- barCode.py
+++ barCode.py
@@ -6,112 +6,72 @@ from objc_util import *
 from ctypes import c_void_p
 import ui
 import sound
-import console
 
-found_codes = set()
-main_view = None
-
-AVCaptureSession = ObjCClass('AVCaptureSession')
-AVCaptureDevice = ObjCClass('AVCaptureDevice')
-AVCaptureDeviceInput = ObjCClass('AVCaptureDeviceInput')
-AVCaptureMetadataOutput = ObjCClass('AVCaptureMetadataOutput')
-AVCaptureVideoPreviewLayer = ObjCClass('AVCaptureVideoPreviewLayer')
-dispatch_get_current_queue = c.dispatch_get_current_queue
-dispatch_get_current_queue.restype = c_void_p
-
-class PopOverView(ui.View):
-    view_main = None
-    view_po = None 
+class QRscanner(self):
+    found_codes = set()   
+    MetadataDelegate = create_objc_class('MetadataDelegate', 
+        methods=[captureOutput_didOutputMetadataObjects_fromConnection_], 
+        protocols=['AVCaptureMetadataOutputObjectsDelegate']
+        )
 
     def __init__(self):
-        PopOverView.view_main = ui.load_view('pop-over')
-        PopOverView.view_main.present('fullscreen')
-        mode = 1
-        if mode == 1:
-            PopOverView.view_po.present('Godkjenn 5S?',popover_location=(400,400))
-        elif mode == 2:
-            PopOverView.view_po.present('Hvordan vil du sende resultatene?',popover_location=(400,400))
-
-        def quit(self, sender):
-            mode = 1
-            def ask_user(self, sender, mode):
-                if mode == 1:
-                    PopOverView.view_po.close()
-                    if sender.name == 'Ja':
-                        PopOverView.view_main.close()
-                        mode = 2
-                        PopOverView()
-                    if sender.name == 'Nei':
-                        PopOverView.view_main.close()
-                elif mode == 2:
-                    PopOverView.view_po.close()
-                    if sender.name == 'iMessage':
-                        PopOverView.view_main.close()
-                        newSMS()
-                    if sender.name == 'E-Mail':
-                        PopOverView.view_main.close()
-                        newSMS()
+        self.main_view = None
+        self.AVCaptureSession = ObjCClass('AVCaptureSession')
+        self.AVCaptureDevice = ObjCClass('AVCaptureDevice')
+        self.AVCaptureDeviceInput = ObjCClass('AVCaptureDeviceInput')
+        self.AVCaptureMetadataOutput = ObjCClass('AVCaptureMetadataOutput')
+        self.AVCaptureVideoPreviewLayer = ObjCClass('AVCaptureVideoPreviewLayer')
+        self.dispatch_get_current_queue = c.dispatch_get_current_queue
+        self.dispatch_get_current_queue.restype = c_void_p
 
 
-            # PopOverView.view_po = ui.load_view('Nei')
+    def captureOutput_didOutputMetadataObjects_fromConnection_(_self, _cmd, _output, _metadata_objects, _conn):
+        objects = ObjCInstance(_metadata_objects)
+        for obj in objects:
+            s = str(obj.stringValue())
+            if s not in found_codes:
+                found_codes.add(s)
+                sound.play_effect('digital:PowerUp7')
+            main_view['label'].text = 'Last scan: ' + s
 
-
-def captureOutput_didOutputMetadataObjects_fromConnection_(_self, _cmd, _output, _metadata_objects, _conn, session):
-    objects = ObjCInstance(_metadata_objects)
-    for obj in objects:
-        s = str(obj.stringValue())
-        if s not in found_codes:
-            found_codes.add(s)
-            sound.play_effect('digital:PowerUp7')
-        main_view['label'].text = 'Last scan: ' + s
-        PopOverView()
-
-def newMail(text):
-    console.alert('Ny mail', hide_cancel_button=True)
-
-def newSMS(text):
-    console.alert('Ny SMS', hide_cancel_button=True)
-
-MetadataDelegate = create_objc_class('MetadataDelegate', methods=[captureOutput_didOutputMetadataObjects_fromConnection_], protocols=['AVCaptureMetadataOutputObjectsDelegate'])
-
-@on_main_thread
-def main():
-    global main_view
-    delegate = MetadataDelegate.new()
-    main_view = ui.View(frame=(0, 0, 400, 400))
-    main_view.name = 'Barcode Scanner'
-    session = AVCaptureSession.alloc().init()
-    device = AVCaptureDevice.defaultDeviceWithMediaType_('vide')
-    _input = AVCaptureDeviceInput.deviceInputWithDevice_error_(device, None)
-    if _input:
-        session.addInput_(_input)
-    else:
-        print('Failed to create input')
-        return
-    output = AVCaptureMetadataOutput.alloc().init()
-    queue = ObjCInstance(dispatch_get_current_queue())
-    output.setMetadataObjectsDelegate_queue_(delegate, queue)
-    session.addOutput_(output)
-    output.setMetadataObjectTypes_(output.availableMetadataObjectTypes())
-    prev_layer = AVCaptureVideoPreviewLayer.layerWithSession_(session)
-    prev_layer.frame = ObjCInstance(main_view).bounds()
-    prev_layer.setVideoGravity_('AVLayerVideoGravityResizeAspectFill')
-    ObjCInstance(main_view).layer().addSublayer_(prev_layer)
-    label = ui.Label(frame=(0, 0, 400, 30), flex='W', name='label')
-    label.background_color = (0, 0, 0, 0.5)
-    label.text_color = 'white'
-    label.text = 'Nothing scanned yet'
-    label.alignment = ui.ALIGN_CENTER
-    main_view.add_subview(label)
-    session.startRunning()
-    main_view.present('sheet')
-    main_view.wait_modal()
-    session.stopRunning()
-    delegate.release()
-    session.release()
-    output.release()
-    if found_codes:
-        print 'All scanned codes:\n' + '\n'.join(found_codes)
+    @on_main_thread
+    def main():
+        global main_view
+        delegate = MetadataDelegate.new()
+        main_view = ui.View(frame=(0, 0, 400, 400))
+        main_view.name = 'Barcode Scanner'
+        session = AVCaptureSession.alloc().init()
+        device = AVCaptureDevice.defaultDeviceWithMediaType_('vide')
+        _input = AVCaptureDeviceInput.deviceInputWithDevice_error_(device, None)
+        if _input:
+            session.addInput_(_input)
+        else:
+            print('Failed to create input')
+            return
+        output = AVCaptureMetadataOutput.alloc().init()
+        queue = ObjCInstance(dispatch_get_current_queue())
+        output.setMetadataObjectsDelegate_queue_(delegate, queue)
+        session.addOutput_(output)
+        output.setMetadataObjectTypes_(output.availableMetadataObjectTypes())
+        prev_layer = AVCaptureVideoPreviewLayer.layerWithSession_(session)
+        prev_layer.frame = ObjCInstance(main_view).bounds()
+        prev_layer.setVideoGravity_('AVLayerVideoGravityResizeAspectFill')
+        ObjCInstance(main_view).layer().addSublayer_(prev_layer)
+        label = ui.Label(frame=(0, 0, 400, 30), flex='W', name='label')
+        label.background_color = (0, 0, 0, 0.5)
+        label.text_color = 'white'
+        label.text = 'Nothing scanned yet'
+        label.alignment = ui.ALIGN_CENTER
+        main_view.add_subview(label)
+        session.startRunning()
+        main_view.present('sheet')
+        main_view.wait_modal()
+        session.stopRunning()
+        delegate.release()
+        session.release()
+        output.release()
+        if found_codes:
+            print('All scanned codes:\n' + '\n'.join(found_codes))
 
 if __name__ == '__main__':
-    main()
+    QRscanner()

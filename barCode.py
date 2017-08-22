bc71c566a9642eff574096b974698c95973f6500
Test <123@test.com>
2017-08-22 10:05:22 +0200

    python3

--- barCode.py
+++ barCode.py
@@ -54,27 +54,7 @@ class PopOverView(ui.View):
 
 
             # PopOverView.view_po = ui.load_view('Nei')
-    
 
-
-# def menu():
-#   global menu_view
-#   menu_view = ui.View()                                           # [1]
-#   menu_view.name = 'Menu'                                         # [2]
-#   menu_view.background_color = 'white'                            # [3]
-#   # QR-knapp
-#   button_QR = ui.Button(title='Scan QR-kode')                 # [4]
-#   button_QR.center = (menu_view.width * 0.5, menu_view.height * 0.25)     # [5]
-#   button_QR.flex = 'LRTB'                                     # [6]
-#   button_QR.action = main                                     # [7]
-#   menu_view.add_subview(button_QR)                                # [8]
-#   # QR-knapp
-#   button_QR = ui.Button(title='Scan QR-kode')                 # [4]
-#   button_QR.center = (menu_view.width * 0.5, menu_view.height * 0.25)     # [5]
-#   button_QR.flex = 'LRTB'                                     # [6]
-#   button_QR.action = main                                     # [7]
-#   menu_view.add_subview(button_QR)                                # [8]
-#   menu_view.present('sheet')                                      # [9]
 
 def captureOutput_didOutputMetadataObjects_fromConnection_(_self, _cmd, _output, _metadata_objects, _conn, session):
     objects = ObjCInstance(_metadata_objects)
@@ -92,50 +72,13 @@ def newMail(text):
 def newSMS(text):
     console.alert('Ny SMS', hide_cancel_button=True)
 
-# def treatScan(found_codes):
-#   if found_codes:
-#       DoneScanning = console.alert('Vil du vurdere flere?' 'Ja', 'Nei', hide_cancel_button=True)
-#       if DoneScanning == 2:
-#           sendMethod = console.alert('Sende resultater','E-post','iMessage')
-#           mytext = found_codes
-#           if sendMethod == 1:
-#               newMail(mytext)
-#           elif sendMethod == 2:
-#               newSMS(mytext)
-#       elif DoneScanning == 1:
-#           main()
-#       print('All scanned codes:\n' + '\n'.join(found_codes))
-
-# def button_tapped(sender):
-#     sender.title = 'Hello'
-
-# def confirmationUI(s):
-#   name = s
-#   view = ui.View()                                      # [1]
-#   view.name = 'Godkjenning'                                  # [2]
-#   view.background_color = 'white'                       # [3]
-#   nameLabel = ui.label(text='Vil du godkjenne {}s 5S?'.format(name))
-#   nameLabel.center = (view.width * 0.5, view.height * 0.1) # [5]
-#   btn_valid = ui.Button(title='5s-vurdering')              # [4]
-#   btn_valid.center = (view.width * 0.25, view.height * 0.5) # [5]
-#   btn_valid.flex = 'LRTB'                                  # [6]
-#   btn_valid.action = button_tapped                         # [7]
-#   view.add_subview(btn_valid)                              # [8]
-#   btn_invalid = ui.Button(title='5s-vurdering')              # [4]
-#   btn_invalid.center = (view.width * 0.75, view.height * 0.5) # [5]
-#   btn_invalid.flex = 'LRTB'                                  # [6]
-#   btn_invalid.action = button_tapped                         # [7]
-#   view.add_subview(btn_invalid)                              # [8]
-#   view.present('sheet')                                 # [9]
-
-
 MetadataDelegate = create_objc_class('MetadataDelegate', methods=[captureOutput_didOutputMetadataObjects_fromConnection_], protocols=['AVCaptureMetadataOutputObjectsDelegate'])
 
 @on_main_thread
 def main():
     global main_view
     delegate = MetadataDelegate.new()
-    main_view = ui.View(frame=(0,0,main_view.width, main_view.height))
+    main_view = ui.View(frame=(0, 0, 400, 400))
     main_view.name = 'Barcode Scanner'
     session = AVCaptureSession.alloc().init()
     device = AVCaptureDevice.defaultDeviceWithMediaType_('vide')
@@ -159,28 +102,16 @@ def main():
     label.text_color = 'white'
     label.text = 'Nothing scanned yet'
     label.alignment = ui.ALIGN_CENTER
-
-    btn_valid = ui.Button(title='Godkjent')
-    btn_valid.center = (view.width * 0.5, view.height * 0.4)
-    btn_valid.flex = 'LRTB'                                  # [6]
-    btn_valid.action = btn_valid_tapped 
-    btn_valid.enabled = False
-    main_view.add_subview(btn_valid)
-
     main_view.add_subview(label)
-
     session.startRunning()
     main_view.present('sheet')
     main_view.wait_modal()
-
     session.stopRunning()
     delegate.release()
     session.release()
     output.release()
-
-
-def btn_valid_tapped(sender):
-    print("Godkjent!")
+    if found_codes:
+        print 'All scanned codes:\n' + '\n'.join(found_codes)
 
 if __name__ == '__main__':
     main()
